6db9109e43715c3ab1606e293d32a46686d207fc
Fándly Gergő <gergo@systemtest.tk>
2019-05-01 15:21:25 +0300

    Added react and compilation with webpack and babel

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const HtmlPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	mode: process.env.NODE_ENV || 'development',
+	devtool: 'source-map',
+	entry: './src/index.js',
+	output: {
+		path: path.join(__dirname, 'build'),
+		filename: 'index.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.jsx?$/,
+				exclude: /node_modules/,
+				use: 'babel-loader'
+			}
+		]
+	},
+	resolve: {
+		extensions: ['.js', '.jsx']
+	},
+	plugins: [
+		new HtmlPlugin({
+			hash: true,
+			filename: 'index.html',
+			template: 'src/public/index.html'
+		})
+	]
+};
